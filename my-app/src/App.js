cd9d7698c12090ef2a61dfc08b797083a9035b7f
hA2zy <haz2y@dsm.hs.kr>
2023-07-28 21:16:11 +0900

    api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <dt>기온</dt>
+        <dd class="temperature">
+        </dd>
+        <dt>위치</dt>
+        <dd class="place"></dd>
+        <dt>설명</dt>
+        <dd class="description"></dd>
+      </div>
+    </>
   );
 }
 
